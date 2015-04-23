af44de40cc3274d49f5c9519dd39f2efda159f71
xchapter7x <xchapter7x@gmail.com>
2015-04-22 20:56:21 -0500

    more POC
    nothing really to see here, just messing with
    the algo

--- machine_handler.go
+++ machine_handler.go
@@ -9,14 +9,6 @@ import (
 	"github.com/arbovm/levenshtein"
 )
 
-func DistanceTry1() {
-	s1 := "kitten"
-	s2 := "sitting"
-	fmt.Printf("The distance between %v and %v is %v\n",
-		s1, s2, levenshtein.Distance(s1, s2))
-	// -> The distance between kitten and sitting is 3
-}
-
 func DistanceTry() {
 	km := NewLogKMeans([]string{"alkjsdf",
 		"lkd",
